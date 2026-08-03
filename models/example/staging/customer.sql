3c8fab9ae095cee2200dc500815a1af0488852f8
RASMIRANJAN PARIDA <parida.rasmi92@gmail.com>
2026-08-03 07:56:14 +0000

    commit

--- models/example/staging/customer.sql
+++ models/example/staging/customer.sql
@@ -1,6 +1,6 @@
 {{config(materialized = 'table')}}
-WITH tb2 as(
+WITH tb5 as(
     select id, first_name, last_name 
     from {{source('Test_dbt','customer_raw_data')}})
 
-select * from tb2
+select * from tb5
